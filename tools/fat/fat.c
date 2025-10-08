4a47932cb0edb392a7c9e692d06a23c8d3734704
root <root@Mael.localdomain>
2025-10-08 12:58:07 +0200

    Implemented fat drives in C to read files

--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 typedef uint8_t bool;
 #define true 1
 #define false 0
-
-
 
 typedef struct
 {
@@ -50,29 +50,21 @@ typedef struct
   uint16_t ModifiedDate;
   uint16_t FirstClusterLow;
   uint32_t Size;
-
 }__attribute__((packed)) DirectoryEntry;
 
-
-
-
-
-
 BootSector g_BootSector;
-uint8_t* g_fat = NULL;
+uint8_t* g_Fat = NULL;
 DirectoryEntry* g_RootDirectory = NULL;
-
+uint32_t g_RootDirectoryEnd;
 
 bool readBootSector(FILE* disk)
 {
 	return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0; 
-
 }
-
 
 bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut){
   bool ok = true;
-  ok = ok && (fseek(disk), lba * g_BootSector.BytesPerSector, SEEK_SET == 0);
+  ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0);
   ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count);
   return ok;
 }
@@ -80,6 +72,48 @@ bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut){
 bool readFat(FILE* disk){
   g_Fat = (uint8_t*) malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector);
   return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat);
+}
+
+bool readRootDirectory(FILE* disk){
+	uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount;
+	uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount;
+	uint32_t sectors = (size / g_BootSector.BytesPerSector);
+	if(size % g_BootSector.BytesPerSector > 0)
+		sectors++;
+  g_RootDirectoryEnd = lba + sectors;
+	g_RootDirectory = (DirectoryEntry*) malloc(sectors * g_BootSector.BytesPerSector); // Treat the memory address returned by malloc as a pointer to a DirectoryEntry object
+	return readSectors(disk, lba, sectors, g_RootDirectory);
+}
+
+DirectoryEntry* findFile(const char* name)
+{
+  for (uint32_t i = 0; i < g_BootSector.DirEntryCount; i++)
+  {
+    if (memcmp(name, g_RootDirectory[i].Name, 11) == 0)
+      return &g_RootDirectory[i];
+  }
+  return NULL;
+}
+
+
+bool readFile(DirectoryEntry* fileEntry, FILE* disk, uint8_t* outputBuffer)
+{
+  bool ok = true;
+
+  uint16_t currentCluster = fileEntry->FirstClusterLow;
+  do {
+
+    uint32_t lba = g_RootDirectoryEnd + (currentCluster -2) * g_BootSector.SectorsPerCluster;
+    ok = ok && readSectors(disk, lba, g_BootSector.SectorsPerCluster, outputBuffer);
+    outputBuffer += g_BootSector.SectorsPerCluster * g_BootSector.BytesPerSector;
+    uint32_t fatIndex = currentCluster * 3 / 2;
+    if (currentCluster % 2 == 0)
+        currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) & 0x0FFF; // removing the top 4 bits we don't need
+    else
+        currentCluster = (*(uint16_t*)(g_Fat + fatIndex)) >>  4; // removing the bottom 4 bits we don't need
+  } while (ok && currentCluster < 0x0FF8);
+
+  return ok;
 }
 
 
@@ -98,7 +132,6 @@ int main(int argc, char** argv)
     return -1;
   }
 
-
   if(!readBootSector(disk)){
     fprintf(stderr, "Could not read boot sector!\n");
     return -2;
@@ -110,7 +143,40 @@ int main(int argc, char** argv)
     return -3;
   }
 
+  if(!readRootDirectory(disk)){
+    fprintf(stderr, "Could not read root directory!\n");
+    free(g_Fat);
+    free(g_RootDirectory);
+    return -4;
+  }
 
+  DirectoryEntry* fileEntry = findFile(argv[2]);
+  if(!fileEntry){
+    fprintf(stderr, "Could not find file %s!\n", argv[2]);
+    free(g_Fat);
+    free(g_RootDirectory);
+    return -5;
+  }
+
+  uint8_t* buffer = (uint8_t*) malloc(fileEntry->Size + g_BootSector.BytesPerSector);
+  if(!readFile(fileEntry, disk, buffer)){
+    fprintf(stderr, "Could not read file %s!\n", argv[2]);
+    free(g_Fat);
+    free(g_RootDirectory);
+    free(buffer);
+    return -5;
+  }
+
+  for(size_t i = 0; i < fileEntry->Size; i++)
+  {
+    if(isprint(buffer[i])) fputc(buffer[i], stdout);
+    else printf("<%02x>", buffer[i]);
+  }
+  printf("\n");
+
+
+  free(buffer);
+  free(g_RootDirectory);
   free(g_Fat);
-	return 0;
+  return 0;
 }
